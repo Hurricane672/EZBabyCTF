0b17790f0e18367f74ef3c4518454169ef4cfdec
EternalMemory672 <274588969@qq.com>
2021-10-10 13:16:59 +0800

    2021.10.10

--- signup.php
+++ signup.php
@@ -1,6 +1,14 @@
 <?php
-if(isset($_POST["name"]&&)){
-
+$user = "ezbabyctf";
+$passwd = "ezbabyctf";
+$db = "ezbabyctf";
+$tb = "users";
+$conn = new mysqli("localhost","root");
+if($coon->connect_error){
+    die("Connection faild.".$coon->connect_error);
+}
+if(isset($_GET["flag"])){
+    echo 2;
 }
 echo $_POST["name"]."</br>";
 echo $_POST["password"]."</br>";
