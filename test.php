0cb29c40900771c624212e463cf07d0905503d4f
EternalMemory672 <274588969@qq.com>
2021-10-16 14:06:12 +0800

    preg_replace

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+$s1 = "version: '2'\nservices:\nweb:\nbuild: .\nports:\n- \"3000:3000\"\ndb:\nimage: \"mongo\"";
+var_dump($s1);
+echo "<br>";
+$s2 = preg_replace('/\d{1,}:\d{1,}/','200:200',$s1);
+var_dump($s2);
+?> 
