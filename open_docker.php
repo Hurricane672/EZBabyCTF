b6ad37a96c9c70f5930b18c1b801419660ae3777
EternalMemory672 <274588969@qq.com>
2021-10-16 20:14:54 +0800

    docker

--- open_docker.php
+++ open_docker.php
@@ -1,16 +1,29 @@
 <?php
-echo $_POST["type_name"];
-echo "<br>";
-echo $_POST["challenge_name"];
-echo "<br>";
+// echo $_POST["type_name"];
+// echo "<br>";
+// echo $_POST["challenge_name"];
+// echo "<br>";
 
 $path="./challenges/" . $_POST["type_name"] . "/" . $_POST["challenge_name"] . "/docker-compose.yml";
-echo $path;
-echo "<br>";
+// echo $path;
+// echo "<br>";
 $content = file_get_contents($path);
-echo $content;
-echo "<br>";
-$content = str_replace('- "3000:3000"','- "3001:3000"',$content);
+// echo $content;
+// echo "<br>";
+
+$Num = mt_rand(20000,30000);
+// echo $Num;
+// echo "<br>";
+
+$before = "/[(\d)*:(\d)*]/" ;
+preg_match($before,$content,$match);
+var_dump( $match);
+echo $match . "<br>";
+// echo $before;
+$after = $Num . ':3000';
+echo $after . "<br>";
+
+$content = str_replace($match,$after,$content);
 echo $content;
 echo "<br>";
 if(!file_put_contents($path,$content))
@@ -22,6 +35,3 @@ $content = file_get_contents($path);
 echo $content;
 echo "<br>";
 echo shell_exec("whoami");
-
-
-?>
