0960f97d10aa04e5ed78b2bb149ee82f50608e9d
EternalMemory672 <274588969@qq.com>
2021-10-17 21:26:28 +0800

    BUG界重大突破

--- Js/core.js
+++ Js/core.js
@@ -1,17 +1,17 @@
-function director(val, flag) {
+function director(val, flag, re) {
     switch (flag) {
         case 0:
-            var re = ["?李在赣神魔，不要瞎搞啊人家会坏掉的qwq", 0];
-            return re;
+            re.content = "?李在赣神魔，不要瞎搞啊人家会坏掉的qwq";
+            re.flag = 0;
+            return;
             break;
         case 1:
             if (val != 0) {
                 var uname = "" + val;
-
             } else {
-                flag--;
-                var re = ["(•́へ•́ ╬)你叫了个寂寞……这样让人家这么称呼你呀", 0];
-                return re;
+                re.content = "(•́へ•́ ╬)你叫了个寂寞……这样让人家这么称呼你呀";
+                re.flag = 0;
+                return;
             }
             $.ajax({
                 url: "signup.php",
@@ -25,11 +25,14 @@ function director(val, flag) {
                 success: function (response) {
                     console.log(response);
                     if (response == "false") {
-                        var re = ["你来晚了，这个用户名已经被注册了哈哈哈哈！", 0];
-                        return re;
+                        re.content = "你来晚了，这个用户名已经被注册了哈哈哈哈！";
+                        re.flag = 0;
+                        return;
+
                     } else {
-                        var re1 = ["(❀｣╹□╹)｣*･我很喜欢这个名字owo一定要记好自己的名字嗷！现在，给自己想一个密码吧w", 1];
-                        return re1;
+                        re.content = uname+"……(❀｣╹□╹)｣*･我很喜欢这个名字owo一定要记好自己的名字嗷！现在，给自己想一个密码吧w";
+                        re.flag = 1;
+                        return;
                     }
                 },
                 error: function () {
@@ -42,8 +45,9 @@ function director(val, flag) {
             if (val != 0) {
                 var uname = "" + val;
             } else {
-                var re = ["你真的是来搞安全的嘛？空密码……这也太不安全了！好好反思下！<(｀^′)>", 1];
-                return re;
+                re.content = "你真的是来搞安全的嘛？空密码……这也太不安全了！好好反思下！<(｀^′)>";
+                re.flag = 1;
+                return;
             }
 
             break;
@@ -51,31 +55,36 @@ function director(val, flag) {
             if (val != 0) {
                 var uname = "" + val;
             } else {
-                var re = ["空空又空空，老调戏人家有什么意思(´•ω•̥`)", 2];
-                return re;
+                re.content = "空空又空空，老调戏人家有什么意思(´•ω•̥`)";
+                re.flag = 2;
+                return;
             }
             break;
         case 10:
-            var re = ["?李在赣神魔，不要瞎搞啊人家会坏掉的qwq", 10];
-            return re;
+            re.content = "?李在赣神魔，不要瞎搞啊人家会坏掉的qwq";
+            re.flag = 10;
+            return;
             break;
         case 11:
             if (val != 0) {
                 var uname = "" + val;
             } else {
-                var re = ["你是故意找茬是不是啊<(｀^′)>", 10];
-                return re;
+                re.content = "你是故意找茬是不是啊<(｀^′)>";
+                re.flag = 10;
+                return;
             }
             break;
         case 12:
             if (val != 0) {
                 var uname = "" + val;
             } else {
-                var re = ["(๑•̌.•̑๑)ˀ̣ˀ̣", 11];
-                return re;
+                re.content = "(๑•̌.•̑๑)ˀ̣ˀ̣";
+                re.flag = 11;
+                return;
             }
             break;
         default:
+            return;
             break;
     }
 }
