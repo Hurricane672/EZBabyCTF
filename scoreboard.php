3b0f96244045be1045d3c79cb1866cad9cb4333c
EternalMemory672 <274588969@qq.com>
2021-11-02 20:21:14 +0800

    scoreboard.php

--- scoreboard.php
+++ scoreboard.php
@@ -8,7 +8,7 @@ if($conn->connect_error){
     die("Connection faild.".$conn->connect_error);
 }
 else{
-    $s1 = "select tname,score from scoreboard";
+    $s1 = "select tname,score from scoreboard where tname!=\"__NONE__\" and tname!=\"__ADMIN__\" order by score";
     $result = array();
     if ($r = $conn->query($s1)) {
         while($row = mysqli_fetch_row($r)){
